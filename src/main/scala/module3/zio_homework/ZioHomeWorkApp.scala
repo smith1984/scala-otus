911dd76e5da050ae40d8d3b66b9b1d2db9dbea9e
mppetrov <mppetrov@beeline.ru>
2022-09-22 07:56:30 +0300

    Home work ZIO

--- src/main/scala/module3/zio_homework/ZioHomeWorkApp.scala
+++ src/main/scala/module3/zio_homework/ZioHomeWorkApp.scala
@@ -6,6 +6,28 @@ import zio.random.Random
 import zio.{ExitCode, URIO}
 
 object ZioHomeWorkApp extends zio.App {
-  override def run(args: List[String]): URIO[Clock with Random with Console, ExitCode] =
-    ???
+  override def run(args: List[String]): URIO[Clock with Random with Console, ExitCode] = {
+    //1 задание
+    //module3.zio_homework.guessProgram.exitCode
+
+    // 2 задание
+    /*val cond: Int => Boolean = v => (v >= 1 && v <= 3)
+
+    lazy val zioEff = for {
+      value <- zio.random.nextIntBetween(1, 11)
+      _ <- zio.console.putStrLn(value.toString)
+    } yield value
+
+    module3.zio_homework.doWhile(zioEff, cond).exitCode*/
+
+    // 3 задание
+    //module3.zio_homework.loadConfigOrDefault.exitCode
+    // 4 задание
+    //module3.zio_homework.app.exitCode
+    //module3.zio_homework.appSpeedUp.exitCode
+
+    // 5,6 задание
+    module3.zio_homework.runApp.exitCode
+
+  }
 }
